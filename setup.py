6f1c197f6385186b61f2b3a765a2da62b5ad6a65
Ithika <156116487+saan-123@users.noreply.github.com>
2024-01-09 23:23:28 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pylearn',
+    version='0.1',
+    packages=find_packages(),
+    # Add other relevant metadata
+    author='saan-123',
+    description='blah blah',
+    # ... other metadata
+)
