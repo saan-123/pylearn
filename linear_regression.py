1f4408a596352f850eccd639e92d5d7abf524401
Ithika <156116487+saan-123@users.noreply.github.com>
2024-01-10 12:56:07 +0530

    linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,28 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def local_regression(x0, X, Y, tau):
+    x0 = [1, x0]
+    X = [[1, i] for i in X]
+    X = np.asarray(X)
+    xw = (X.T) * np.exp(np.sum((X - x0) ** 2, axis=1) / (-2 * tau))
+    beta = np.linalg.pinv(xw @ X) @ xw @ Y @ x0
+    return beta
+def draw(tau):
+    prediction = [local_regression(x0, X, Y, tau) for x0 in domain]
+    plt.plot(X, Y, 'o', color='black')
+    plt.plot(domain, prediction, color='red')
+    plt.show()
 
-class LinearRegression:
-    def __init__(self):
-        self.X = np.linspace(-3, 3, num=1000)
-        self.domain = self.X
-        self.Y = np.log(np.abs(self.X ** 2 - 1) + .5)
+X = np.linspace(-3, 3, num=1000)
+domain = X
+Y = np.log(np.abs(X ** 2 - 1) + .5)
 
-    def local_regression(self, x0, tau):
-        x0 = [1, x0]
-        X = [[1, i] for i in self.X]
-        X = np.asarray(X)
-        Xw = (X.T) * np.exp(np.sum((X - x0) ** 2, axis=1) / (-2 * tau))
-        beta = np.linalg.pinv(Xw @ X) @ Xw @ self.Y @ x0
-        return beta
-
-    def draw(self, tau):
-        prediction = [self.local_regression(x0, tau) for x0 in self.domain]
-        plt.plot(self.X, self.Y, 'o', color='black')
-        plt.plot(self.domain, prediction, color='red')
-        plt.show()
-
-# Usage example
-#lr = LinearRegression()
-#lr.draw(10)
-#lr.draw(0.1)
-#lr.draw(0.001)
+draw(10)
+draw(0.1)
+draw(0.001)
